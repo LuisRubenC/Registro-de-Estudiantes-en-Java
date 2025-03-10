55a4e00b6108fe6367864cfe5c369dafd7a35ca9
LuisRubenC <lrcastillo03@hotmail.com>
2025-03-09 23:52:30 -0600

    Implementación de función para registrar estudiantes
    Cree la función que registra un estudiante dado por el usuario donde colocara el nombre, la edad y carrera del estudiante.
    
    Importe la biblioteca "Random" para asignarle a los estudiantes una matricula diferente a cada uno.
    
    Cambie en la clase Estudiante la matricula, pasando de ser un String a un Int.

--- Registro.java
+++ Registro.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
+import java.util.Random;
 
 class Estudiantes{
     String nombre;
     int edad;
-    String matricula;
+    int matricula;
     String carrera;
+
 }
 
-public class App {
+public class Registro {
+
     public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     Estudiantes[] estudiantes = new Estudiantes[100];
     int numEstudiantes=0;
     int op=0;
+    int flag=0;
 
     do{
         System.out.println("---------- BIENVENIDO ----------");
@@ -24,11 +28,16 @@ public class App {
         System.out.println("5.- Salir");
         System.out.println("Elija la opcion que guste...");
         op= scanner.nextInt();
+        scanner.nextLine();
 
         switch(op){
             case 1:
+                flag=RegistrarEstudiante(scanner,numEstudiantes,estudiantes);
+                if(flag==0)
+                numEstudiantes++;
                 break;
             case 2:
+                
                 break;
             case 3:
                 break;
@@ -44,6 +53,33 @@ public class App {
 
     }while(op != 5);
 
+    scanner.close();
+    }
+
+    public static int RegistrarEstudiante(Scanner scanner,int numEstudiantes, Estudiantes[] estudiantes){
+        
+        int i=0;
+        Random random = new Random();
+        int numeroAleatorio = 1000 + random.nextInt(9000); // Genera entre 1000 y 9999
+        
+        if(numEstudiantes>=99){
+            System.out.println("No es posible agregar un estudiante mas, borre alguno y vuelva a intentar...");
+            return 1;
+        }
+
+        Estudiantes nuevoEstudiante = new Estudiantes();
+        System.out.println("Ingrese el nombre del estudiante: ");
+        nuevoEstudiante.nombre=scanner.nextLine();
+        System.out.println("Ingrese la edad del estudiante: ");
+        nuevoEstudiante.edad=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Ingrese la carrera del estudiante: ");
+        nuevoEstudiante.carrera=scanner.nextLine();
+        nuevoEstudiante.matricula=20250000 + numeroAleatorio;
+        estudiantes[numEstudiantes]=nuevoEstudiante;
+        System.out.println("Estudiante registrado correctamente... ");
+
+        return 0;
     }
 
 }
